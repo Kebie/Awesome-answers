d5e98b5b5e05d309bbae38daafa60dbe878f5984
Kebie <nigel.kehler@gmail.com>
2014-03-25 09:14:46 -0700

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def about
+    render text: "I AM SNORTING (ROUTING) RAILS"
+  end
+end
---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,27 @@
+class QuestionsController < ApplicationController
+  #index is the standard for displaying all the questions
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def show
+    @id = params[:id]
+  end
+
+  def edit
+    @id = params[:id]
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    @id = params[:id]
+  end
+
+end
