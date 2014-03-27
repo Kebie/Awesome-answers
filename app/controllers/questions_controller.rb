baa39d9bebbe2b9d939fe74e1a97c0db5f7bc049
Kebie <nigel.kehler@gmail.com>
2014-03-27 15:28:02 -0700

    added vote counting

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,9 @@
 class QuestionsController < ApplicationController
+
+  before_action :find_question,
+                  only: [:show, :edit, :destroy, :update, :vote_up, :vote_down]
+  #before_action :find_question, except: [:index, :new, :destroy, :update]
+
   #index is the standard for displaying all the questions
   def index
     @questions = Question.all    
@@ -14,19 +19,34 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @id = params[:id]
   end
 
   def edit
-    @id = params[:id]
   end
 
   def update
-    
+    if @question.update_attributes(question_attributes)
+      redirect_to @question, notice: "Your question was updated!"
+    else
+      render :edit
+    end
+  end
+
+  def vote_up
+    @question.increment!(:vote_count) # ! auto saves to database
+    session[:has_voted] = true
+    redirect_to @question
+  end
+
+  def vote_down
   end
 
   def destroy
-    @id = params[:id]
+    if @question.destroy
+      redirect_to questions_path, notice: "Question deleted"
+    else
+      redirect_to question_path, error: "We had a problem deleting your question"
+    end
   end
 
   def new
@@ -38,5 +58,10 @@ class QuestionsController < ApplicationController
   def question_attributes
     params.require(:question).permit([:title,:description]) 
   end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140325170040) do
+ActiveRecord::Schema.define(version: 20140327181158) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20140325170040) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "vote_count",  default: 0
   end
 
 end
